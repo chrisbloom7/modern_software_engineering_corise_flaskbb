6ffa51deb1ecac870866593fa50cd2303fb1cc49
Chris Bloom <chrisbloom7@github.com>
2023-07-03 19:28:11 +0000

    Reformat terraform files

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -2,7 +2,7 @@ resource "aws_elastic_beanstalk_application" "application" {
   name = "flaskbb"
 }
 resource "aws_elastic_beanstalk_environment" "environment" {
-     name                = "flaskbb-environment"
+  name                = "flaskbb-environment"
   cname_prefix        = "corisechrisbloom7flaskbb"
   application         = aws_elastic_beanstalk_application.application.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Python 3.11"
