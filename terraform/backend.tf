a9de22666a960ed6c52f08f1aa11c40296ae24c3
Chris Bloom <chrisbloom7@github.com>
2023-07-03 18:54:06 +0000

    Add step to fail format workflow if format check fails

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-flaskbb-corise-chrisbloom7"
     key    = "core/terraform.tfstate"
-    region = "us-east-1"
+         region = "us-east-1"
   }
 }
